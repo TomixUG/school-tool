b66db6149202ca9cdfaa70d09e3bd4dca313ea14
TomixUG <tomas.sousek1@gmail.com>
2021-07-14 23:55:51 +0200

    add flashcards play logic

---
+++ client/src/views/pages/flashcards/Play/Logic.jsx
@@ -0,0 +1,144 @@
+import React, { useState } from 'react';
+import { Card, CardBody, Row, Col, Button, Alert, Spinner } from 'reactstrap';
+import { useMutation, gql } from '@apollo/client';
+
+import { X, Check } from 'react-feather';
+
+const MODIFY_CARD = gql`
+  mutation modifyCard($cardDeckId: String!, $cardId: String!, $timeSpent: Float, $status: CardStatus) {
+    modifyCard(cardDeckId: $cardDeckId, cardId: $cardId, timeSpent: $timeSpent, status: $status)
+  }
+`;
+
+function Logic({ data, cardDeckId }) {
+  const [index, setIndex] = useState(0);
+  const [opened, setOpened] = useState(false);
+
+  var [modifyCard, { data: _, loading, error }] = useMutation(MODIFY_CARD, {
+    onCompleted() {
+      next();
+    },
+    onError() {},
+  });
+
+  function prev() {
+    if (index - 1 >= 0) {
+      setIndex(index - 1);
+      setOpened(false);
+    }
+  }
+  function next() {
+    if (index + 1 < data.getCardDeck.cards.length) {
+      setIndex(index + 1);
+      setOpened(false);
+    }
+  }
+
+  return (
+    <div>
+      {error ? <Alert color="danger">{error.message}</Alert> : ''}
+      <Row>
+        <Col>
+          <p>{index + 1 + '/' + data.getCardDeck.cards.length}</p>
+        </Col>
+        <Col>
+          {(() => {
+            var color;
+            switch (data.getCardDeck.cards[index].status) {
+              case 'UNPLAYED':
+                color = 'gray';
+                break;
+              case 'GOOD':
+                color = 'green';
+                break;
+              case 'BAD':
+                color = 'red';
+                break;
+            }
+            return <p style={{ textAlign: 'right', color: color, fontSize: '20' }}>⬤</p>;
+          })()}
+        </Col>
+      </Row>
+      <Card>
+        <CardBody>
+          <div style={{ textAlign: 'center' }}>
+            <h3>{data.getCardDeck.cards[index].front}</h3>
+            {opened ? (
+              <>
+                <hr />
+                <h3>{data.getCardDeck.cards[index].back}</h3>
+              </>
+            ) : (
+              ''
+            )}
+          </div>
+        </CardBody>
+      </Card>
+
+      <Row>
+        <Col xs="12" md="4">
+          <Button className="float-md-left" onClick={prev} disabled={index - 1 >= 0 ? false : true}>
+            Prev
+          </Button>
+        </Col>
+        <Col xs="12" md="4">
+          {!opened ? (
+            <Button className="float-md-middle" color="primary" onClick={() => setOpened(true)}>
+              Reveal
+            </Button>
+          ) : (
+            <>
+              {loading ? (
+                <Spinner color="primary" />
+              ) : (
+                <>
+                  <Button
+                    color="success"
+                    onClick={() => {
+                      modifyCard({
+                        variables: {
+                          cardDeckId: cardDeckId,
+                          cardId: data.getCardDeck.cards[index].id,
+                          status: 'GOOD',
+                        },
+                      });
+                    }}
+                  >
+                    <Check />
+                    Good
+                  </Button>
+                  <Button
+                    color="danger"
+                    onClick={() => {
+                      modifyCard({
+                        variables: {
+                          cardDeckId: cardDeckId,
+                          cardId: data.getCardDeck.cards[index].id,
+                          status: 'BAD',
+                        },
+                      });
+                    }}
+                  >
+                    <X />
+                    Bad
+                  </Button>
+                </>
+              )}
+            </>
+          )}
+        </Col>
+        <Col xs="12" md="4">
+          <Button
+            className="float-md-right"
+            onClick={next}
+            disabled={index + 1 < data.getCardDeck.cards.length ? false : true}
+          >
+            Skip
+          </Button>
+        </Col>
+      </Row>
+    </div>
+  );
+}
+
+export default Logic;
--- client/src/views/pages/flashcards/Play/Play.jsx
+++ client/src/views/pages/flashcards/Play/Play.jsx
@@ -1,9 +1,95 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+import { Card, Row, Col, CardBody, Button, Alert } from 'reactstrap';
+import { X } from 'react-feather';
+import { useHistory } from 'react-router-dom';
+import Logic from './Logic';
+
+import PageLoading from './../../../util/PageLoading';
+
+const GET_CARD_DECK = gql`
+  query getCardDeck($cardDeckId: String!) {
+    getCardDeck(cardDeckId: $cardDeckId) {
+      name
+      numberOfCards
+      cards {
+        id
+        front
+        back
+        timeSpent
+        status
+      }
+    }
+  }
+`;
 
 function Play(props) {
   const cardDeckId = props.match.params.id;
+  let history = useHistory();
 
-  return <div></div>;
+  const [sorted, setSorted] = useState(false);
+
+  const { loading, error, data } = useQuery(GET_CARD_DECK, {
+    variables: { cardDeckId: cardDeckId },
+    onCompleted() {
+      //sort the cards
+      var unplayed = [];
+      var good = [];
+      var bad = [];
+
+      for (var i = 0; i < data.getCardDeck.cards.length; i++) {
+        if (data.getCardDeck.cards[i].status === 'UNPLAYED') unplayed.push(data.getCardDeck.cards[i]);
+        if (data.getCardDeck.cards[i].status === 'GOOD') good.push(data.getCardDeck.cards[i]);
+        if (data.getCardDeck.cards[i].status === 'BAD') bad.push(data.getCardDeck.cards[i]);
+      }
+
+      data.getCardDeck.cards = unplayed.concat(bad, good);
+
+      setSorted(true);
+    },
+  });
+
+  if (loading) return <PageLoading />;
+  if (error) return <Alert color="danger">{error.message}</Alert>;
+
+  return (
+    <div>
+      <Row>
+        <Col xs="12" md="12">
+          <Card>
+            <CardBody>
+              <Row>
+                <Col xs="12" md="6">
+                  <h4 className="float-md-left">{data.getCardDeck.name}</h4>
+                </Col>
+                <Col xs="12" md="6">
+                  <span className="float-md-right ">
+                    <Button
+                      color="danger"
+                      size="sm"
+                      onClick={() => {
+                        history.push(`/flashcards/deck/${cardDeckId}`);
+                      }}
+                    >
+                      <X /> Stop learning
+                    </Button>
+                  </span>
+                </Col>
+              </Row>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+
+      {data.getCardDeck.cards.length === 0 ? (
+        <h3 className="m-t-xxl text-center">
+          <b>No cards have been added yet</b>
+        </h3>
+      ) : (
+        <>{sorted ? <Logic data={data} cardDeckId={cardDeckId} /> : 'sorting ...'}</>
+      )}
+    </div>
+  );
 }
 
 export default Play;
